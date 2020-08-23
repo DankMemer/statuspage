45a7d3ed35202d2db1d95fddff208f1ed261b764
aetheryx <zainalih3c@gmail.com>
2020-08-23 02:35:48 +0200

    a

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
----
 welcome to garbage pile aetheryx cant code sry
-test
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import App from './App.svelte';
 
 const app = new App({
 	target: document.body,
-	a: 'b'
 });
 
 export default app;
